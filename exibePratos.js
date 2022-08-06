67a55956424798358e196257a103c1449422431f
AntonioNasi <86308977+AntonioNasi@users.noreply.github.com>
2022-08-06 12:38:35 -0300

    Add files via upload

---
+++ exibePratos.js
@@ -0,0 +1,12 @@
+function exibe0() {
+    var mostra0 = document.getElementById('pratos0')
+    var fechar0 = document.getElementById('fechar0')
+    mostra0.style.display='block'
+    fechar0.style.display='block'
+
+}
+
+function esconde(){
+    var escondePratos = document.getElementById('pratos0')
+    escondePratos.style.display='none'
+}
