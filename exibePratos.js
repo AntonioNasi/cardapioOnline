1e56bb48ba93745b833c56af0d086d0f3a81b202
AntonioNasi <86308977+AntonioNasi@users.noreply.github.com>
2022-08-08 17:27:29 -0300

    Add files via upload

--- exibePratos.js
+++ exibePratos.js
@@ -1,3 +1,5 @@
+// Sugestões do chefe
+
 function exibe0() {
     var mostra0 = document.getElementById('pratos0')
     var fechar0 = document.getElementById('fechar0')
@@ -8,5 +10,20 @@ function exibe0() {
 
 function esconde(){
     var escondePratos = document.getElementById('pratos0')
+    escondePratos.style.display='none'
+}
+
+// Massas especiais
+
+function exibe1() {
+    var mostra1 = document.getElementById('pratos1')
+    var fechar1 = document.getElementById('fechar1')
+    mostra1.style.display='block'
+    fechar1.style.display='block'
+
+}
+
+function esconde1(){
+    var escondePratos = document.getElementById('pratos1')
     escondePratos.style.display='none'
 }
